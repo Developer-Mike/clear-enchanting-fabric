3302e603bb9e49ae32327197423c7b8ad952291e
delta <mika.schlup@gmail.com>
2023-07-05 15:55:17 +0200

    Enchantments are now centered. Better visibility because of new text color.

--- src/main/java/com/mikadev/clearenchanting/mixin/EnchantingTableScreenMixin.java
+++ src/main/java/com/mikadev/clearenchanting/mixin/EnchantingTableScreenMixin.java
@@ -6,11 +6,12 @@ import net.minecraft.client.gui.screen.ingame.EnchantmentScreen;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.text.StringVisitable;
 import net.minecraft.text.Style;
-import net.minecraft.text.Text;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
@@ -26,8 +27,20 @@ public class EnchantingTableScreenMixin {
 
         Enchantment enchantment = Enchantment.byRawId(enchantmentId);
         if (enchantment == null) return parent.generatePhrase(textRenderer, width);
-        Text enchantmentName = Enchantment.byRawId(enchantmentId).getName(enchantmentLevel);
+        String enchantmentName = Enchantment.byRawId(enchantmentId).getName(enchantmentLevel).getString();
 
-        return textRenderer.getTextHandler().trimToWidth(enchantmentName, width, Style.EMPTY);
+        return textRenderer.getTextHandler().trimToWidth(StringVisitable.styled(enchantmentName, Style.EMPTY), width * 2, Style.EMPTY);
+    }
+
+    @ModifyArgs(method = "drawBackground", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWrapped(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/StringVisitable;IIII)V"))
+    private void onDrawText(Args args) {        
+        TextRenderer textRenderer = (TextRenderer) args.get(0);
+        StringVisitable text = (StringVisitable) args.get(1);
+
+        int yMod = textRenderer.getWidth(text) <= 70 ? 3 : -1;
+        args.set(3, (int) args.get(3) + yMod);
+
+        args.set(4, 70);
+        args.set(5, 0xFFFFFFFF);
     }
 }
